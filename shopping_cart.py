f9561c79a01763d13b3d78d08513f5c2029592a6
jsr512 <50751736+jsr512@users.noreply.github.com>
2019-06-14 10:36:26 -0400

    Printe Receipt w/ Total and Products

--- shopping_cart.py
+++ shopping_cart.py
@@ -33,21 +33,30 @@ products = [
 #Info Capture / Input
 #
 
+total_price = 0
+selected_ids = []
+
 while True: 
     selected_id = input('Please input a product indentifier: ')
     
     if selected_id == "DONE":
         break
     
-    matching_products = [p for p in products if str(p[('id')]) == str(selected_id)]
-    matching_product = matching_products[0]
-    print("Selected Product: " + matching_product["name"] + " " + str(matching_product["price"]))
+    else:
+        selected_ids.append(selected_id) 
+
 
 #
 #Infor Display / Output
 #
 
+for selected_id in selected_ids:
+    matching_products = [p for p in products if str(p[('id')]) == str(selected_id)]
+    matching_product = matching_products[0]
+    total_price = total_price + matching_product["price"]
+    print("Selected Product: " + matching_product["name"] + " " + str(matching_product["price"]))
 
+print("Total Price: " + str(total_price)) #convert to USD
 
 #A grocery store name of your choice
 #A grocery store phone number and/or website URL and/or address of choice
