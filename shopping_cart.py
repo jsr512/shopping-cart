3cd268cc3599d899d334c6749f16c2fefd518e6d
jsr512 <50751736+jsr512@users.noreply.github.com>
2019-06-14 09:56:58 -0400

    Print Product by Supplied ID

--- shopping_cart.py
+++ shopping_cart.py
@@ -34,8 +34,9 @@ products = [
 #
 
 selected_id = input('Please input a product indentifier: ')
-print(selected_id)
-print(type(selected_id))
+matching_products = [p for p in products if str(p[('id')]) == str(selected_id)]
+matching_product = matching_products[0]
+print("Selected Product: " + matching_product["name"] + " " + str(matching_product["price"]))
 
 #
 #Infor Display / Output
@@ -49,8 +50,4 @@ print(type(selected_id))
 #The amount of tax owed (e.g. $0.39), calculated by multiplying the total cost by a New York City sales tax rate of 8.75% (for the purposes of this project, groceries are not exempt from sales tax)
 #The total amount owed, formatted as US dollars and cents (e.g. $4.89), calculated by adding together the amount of tax owed plus the total cost of all shopping cart items
 #A friendly message thanking the customer and/or encouraging the customer to shop again
-
-
-
-
 
