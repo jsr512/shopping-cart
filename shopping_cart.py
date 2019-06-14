d0b4047a003a57caea42aefaaaee65cce8c9232d
jsr512 <50751736+jsr512@users.noreply.github.com>
2019-06-14 12:00:13 -0400

    Add Date, Header, Price, and Tax Formatted

--- shopping_cart.py
+++ shopping_cart.py
@@ -1,6 +1,4 @@
-# shopping_cart.py
-
-from pprint import pprint
+import datetime 
 
 products = [
     {"id":1, "name": "Chocolate Sandwich Cookies", "department": "snacks", "aisle": "cookies cakes", "price": 3.50},
@@ -25,16 +23,13 @@ products = [
     {"id":20, "name": "Pomegranate Cranberry & Aloe Vera Enrich Drink", "department": "beverages", "aisle": "juice nectars", "price": 4.25}
 ] # based on data from Instacart: https://www.instacart.com/datasets/grocery-shopping-2017
 
-#print(products)
-#pprint(products)
-
-
 #
 #Info Capture / Input
 #
 
 total_price = 0
 selected_ids = []
+now = datetime.datetime.now()
 
 while True: 
     selected_id = input('Please input a product indentifier: ')
@@ -45,24 +40,39 @@ while True:
     else:
         selected_ids.append(selected_id) 
 
-
 #
 #Infor Display / Output
 #
+
+print("------------------")
+print("Jim's NYU Grocery")
+print("555-555-5555")
+print("www.JimsGrocery.nyu")
+print(str(now))
+print("------------------")
+print(" ")
+ 
 
 for selected_id in selected_ids:
     matching_products = [p for p in products if str(p[('id')]) == str(selected_id)]
     matching_product = matching_products[0]
     total_price = total_price + matching_product["price"]
-    print("Selected Product: " + matching_product["name"] + " " + str(matching_product["price"]))
+    print("- " + matching_product["name"] + " " + "${0:.2f}".format((matching_product["price"])))
 
-print("Total Price: " + str(total_price)) #convert to USD
+tax = total_price * .0875
+final_price = (total_price + tax)
 
-#A grocery store name of your choice
-#A grocery store phone number and/or website URL and/or address of choice
-#The date and time of the beginning of the checkout process, formatted in a human-friendly way (e.g. 2019-06-06 11:31 AM)
-#The name and price of each shopping cart item, price being formatted as US dollars and cents (e.g. $1.50)
-#The total cost of all shopping cart items, formatted as US dollars and cents (e.g. $4.50), calculated as the sum of their prices
+print("-----------")
+print("Total Price of Items: " + "$" + "{0:.2f}".format((total_price))) 
+print("-----------")
+print(" ")
+
+print("Tax: " + "$" "{0:.2f}".format(tax))
+print(" ")
+
+print("Final Amount Due: " + "$" + "{0:.2f}".format(final_price))
+
+
 #The amount of tax owed (e.g. $0.39), calculated by multiplying the total cost by a New York City sales tax rate of 8.75% (for the purposes of this project, groceries are not exempt from sales tax)
 #The total amount owed, formatted as US dollars and cents (e.g. $4.89), calculated by adding together the amount of tax owed plus the total cost of all shopping cart items
 #A friendly message thanking the customer and/or encouraging the customer to shop again
